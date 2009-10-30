c7b36f84ec93f2efbf9981a8d2a0e602de89088a
Jon Hoffman <jonhoffman@gmail.com>
2009-10-29 20:21:21 -0400

    updated docs and publishing

--- project/build/YuiCompressorPluginProject.scala
+++ project/build/YuiCompressorPluginProject.scala
@@ -2,4 +2,9 @@ import sbt._
 class YuiCompressorPluginProject(info: ProjectInfo) extends PluginProject(info) {
     val yuiCompressor = "com.yahoo.platform.yui" % "yuicompressor" % "2.3.6"
     val rhino = "rhino" % "js" % "1.6R7"
+	
+	override def managedStyle = ManagedStyle.Maven
+	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+	
 }
