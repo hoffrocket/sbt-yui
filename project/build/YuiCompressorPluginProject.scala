689df03b1213fffdd82e038ce1fbc62b00166f90
Jon Hoffman <jonhoffman@gmail.com>
2010-09-21 10:40:41 -0400

    updated for new yuicompressor group id and bumped version numbers.  also affirmed usability in 2.8.0

--- project/build/YuiCompressorPluginProject.scala
+++ project/build/YuiCompressorPluginProject.scala
@@ -1,11 +1,9 @@
 import sbt._
 class YuiCompressorPluginProject(info: ProjectInfo) extends PluginProject(info) {
-    val yuiCompressor = "net.alchim31.3rd.com.yahoo.platform.yui" % "yuicompressor" % "2.4.2" % "compile->default"
-	
-	val sonaTypeReo = "Sonatype Repo" at "http://oss.sonatype.org/content/groups/github/"
-	
-	override def managedStyle = ManagedStyle.Maven
-	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
-	
+  val yuiCompressor = "com.yahoo.platform.yui" % "yuicompressor" % "2.4.2" % "compile->default"
+  
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  
 }
